1b1a61d65977c1a2da25a0133ef3d96a476b784a
Matija Žagar <zagar.zg@gmail.com>
2022-05-07 11:59:29 +0200

    Configure rollup.config to use environment variables

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,13 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import { config } from "dotenv";
+import replace from '@rollup/plugin-replace';
+
+const configToReplace = {};
+for (const [key, v] of Object.entries(config().parsed)) {
+  configToReplace[`process.env.${key}`] = `'${v}'`;
+}
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -46,6 +53,11 @@ export default {
 				dev: !production
 			}
 		}),
+		replace({
+			include: ["src/**/*.ts", "src/**/*.svelte"],
+			preventAssignment: true,
+			values: configToReplace
+		  }),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
